f6eabc08d593f206b0089805c2f8d0de060a61ba
sooksinyip <sooksinyip@gmail.com>
2015-02-16 15:00:22 +0700

    GUI Design

---
+++ app/src/main/java/com/sooksinyip/weightwatch/MainActivity.java
@@ -0,0 +1,106 @@
+package com.sooksinyip.weightwatch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    CourseDBHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // This method is called when this activity is put foreground.
+
+    }
+
+    public void buttonClicked(View v) {
+        int id = v.getId();
+        Intent i;
+
+        switch(id) {
+            case R.id.btAdd:
+                i = new Intent(this, AddCourseActivity.class);
+                startActivityForResult(i, 88);
+                break;
+
+            case R.id.btShow:
+                i = new Intent(this, ListCourseActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.btSetting:
+
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 88) {
+            if (resultCode == RESULT_OK) {
+                String code = data.getStringExtra("code");
+                int credit = data.getIntExtra("credit", 0);
+                String grade = data.getStringExtra("grade");
+
+            }
+        }
+
+        Log.d("course", "onActivityResult");
+    }
+
+    double gradeToValue(String g) {
+        if (g.equals("A"))
+            return 4.0;
+        else if (g.equals("B+"))
+            return 3.5;
+        else if (g.equals("B"))
+            return 3.0;
+        else if (g.equals("C+"))
+            return 2.5;
+        else if (g.equals("C"))
+            return 2.0;
+        else if (g.equals("D+"))
+            return 1.5;
+        else if (g.equals("D"))
+            return 1.0;
+        else
+            return 0.0;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
